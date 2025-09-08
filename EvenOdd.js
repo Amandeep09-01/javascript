e338b8daeb517dd4fac0cb9b138c1c2fb4330eae
Amandeep09-01 <amandeepkushwaha0901@gmail.com>
2025-09-08 08:51:19 +0530

    Add files via upload

---
+++ EvenOdd.js
@@ -0,0 +1,7 @@
+let a;
+a=prompt("Enter the number");
+if(a%2==0){
+    console.log("Even number");
+}else{
+    console.log("ODD number");
+}
---
+++ LargestThreeNum.js
@@ -0,0 +1,18 @@
+let a=12;
+let b=33;
+let c= 9;
+if(a>b && a>c)
+{
+    console.log("a is the largest");
+}
+else if(b>a && b>c)
+{
+    console.log("b is the largest");
+}
+else if(c>a && c>b)
+{
+    console.log("c is the largest");
+}
+else{
+    console.log("number are equal");
+}
