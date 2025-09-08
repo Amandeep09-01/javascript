d46a77d1d54a32f527ad4cfd3242dff2b68d2d1f
Amandeep09-01 <amandeepkushwaha0901@gmail.com>
2025-09-08 09:20:12 +0530

    Add files via upload

---
+++ PrimeNum.js
@@ -0,0 +1,11 @@
+const isPrimeNumber =(number)=>{
+    if(number<2){
+        return false;
+    }
+    for(let i=2; i<Math.sqrt(number);i++){
+        if(number%i=== 0)
+            return false;
+    }
+    return true;
+};
+console.log("Result", isPrimeNumber(3));
